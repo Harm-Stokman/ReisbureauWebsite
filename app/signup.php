2f39c35aa5f3800d560a98a2010e5b0984cc2d73
Harm-Stokman <harmstok2006@gmail.com>
2026-05-18 15:50:23 +0200

    toevoeging database en pagina's

--- app/signup.php
+++ app/signup.php
@@ -3,16 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vaygo</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
 </head>
 <body>
-    <header>
-
-    </header>
-
-    <footer>
-        
-    </footer>
+    
 </body>
 </html>
