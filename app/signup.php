003b084031611ae0d5c5796f3ed1a0fb44c91a58
Harm-Stokman <harmstok2006@gmail.com>
2026-06-02 13:38:20 +0200

    Functionaliteit aanmelden toegevoegd

--- app/signup.php
+++ app/signup.php
@@ -2,6 +2,25 @@
 session_start();
 include_once 'includes/pdo.php';
 
+if (isset($_POST['submit'])) {
+    if ($_POST['passwordcheck'] == $_POST['password']) {
+
+    $sqluseradd = "INSERT INTO Gebruikers (`Gebruikersnaam`, `Emailadres`, `Wachtwoord`) VALUES (?, ?, ?)";
+
+    $useradd = $pdo->prepare($sqluseradd);
+    $useradd->bindParam(1, $_POST['username']);
+    $useradd->bindParam(2, $_POST['email'] );
+    $useradd->bindParam(3, $_POST['password']);
+    $useradd->execute();    
+    header('Location: index.php');
+    } else {
+        echo "Wachtwoord komt niet overeen!";
+    }
+}
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -31,12 +50,12 @@ include_once 'includes/pdo.php';
         <span class="title-block">
             Aanmelden
         </span>
-        <form class="userform">
+        <form class="userform" method="post">
             <div>
-            <input type="text" name="username" placeholder="Uw gebruikersnaam">
-            <input type="email" name="email" placeholder="Uw E-mailadres">
-            <input type="password" name="password" placeholder="Wachtwoord">
-            <input type="password" name="passwordcheck" placeholder="Wachtwoord herhalen">
+            <input type="text" name="username" placeholder="Uw gebruikersnaam" required>
+            <input type="email" name="email" placeholder="Uw E-mailadres" required>
+            <input type="password" name="password" placeholder="Wachtwoord" required>
+            <input type="password" name="passwordcheck" placeholder="Wachtwoord herhalen" required>
             </div>
             <input class="action-button" type="submit" name="submit" value="Aanmelden">
         </form>
