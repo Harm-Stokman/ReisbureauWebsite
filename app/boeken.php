7662984d826c63357f599ee376e045d870182580
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-06-12 12:44:10 +0200

    Boeken.php en privacypolicy.php af
    Privacy policy afgemaakt.
    Boeken is af. Er moet alleen nog php toegevoegd worden.

--- app/boeken.php
+++ app/boeken.php
@@ -10,7 +10,11 @@ include_once 'includes/pdo.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Boeken</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -24,10 +28,69 @@ include_once 'includes/pdo.php';
 
     </header>
 
+    <?php 
+    
+    $sqlreis = "SELECT * FROM Reizen WHERE Reis_id = ?";
 
+    $reisstatement = $pdo->prepare($sqlreis);   // Maak verbinding met de database en bereid de SQL statement voor
+    $reisstatement->bindParam(1, $_GET['id']);  // Krijg ID van de link waar je op hebt geklikt (bindParam als placeholder)
+    $reisstatement->execute();                  // Voer de SQL statement uit
+
+    $reis = $reisstatement->fetch();
+    
+    ?>
 
     <main>
-
+        <form>
+            <div class="boeken-header">
+                <span class="title-block">Boek nu uw reis naar <?php echo $reis['Bestemming'] ?></span>
+                <div class="boeken-vacations">
+                    <span class="title-block-alt">Boek info</span>
+                    <select class="dropdown-selection">
+                        <option value="">Aantal personen</option>
+                        <option value="1-persoon">1 persoon</option>
+                        <option value="2-personen">2 personen</option>
+                        <option value="3-personen">3 personen</option>
+                        <option value="4-personen">4 personen</option>
+                        <option value="5-personen">5 personen</option>
+                        <option value="6-personen">6 personen</option>
+                    </select>
+                    <input class="dropdown-selection" type="date" name="datum">
+                    <select class="dropdown-selection">
+                        <option value="">Duur</option>
+                        <option value="3-dagen">3 dagen</option>
+                        <option value="5-dagen">5 dagen</option>
+                        <option value="1-week">1 week</option>
+                        <option value="2-weken">2 weken</option>
+                        <option value="3-weken">3 weken</option>
+                        <option value="4-weken">4 weken</option>
+                    </select>
+                </div>
+            </div>
+            <div class="last-check">
+                <span class="title-block">Last check</span>
+                <div class="last-info-boeken">
+                    <div class="boeken-small-info">
+                        <span class="about-us-title">Aantal personen:</span>
+                        <p>1 persoon</p>
+                    </div>
+                    <div class="boeken-small-info">
+                        <span class="about-us-title">Datum van vertrek:</span>
+                        <p>24-08-2026</p>
+                    </div>
+                    <div class="boeken-small-info">
+                        <span class="about-us-title">Duur van vakantie:</span>
+                        <p>3 weken</p>
+                    </div>
+                </div>
+                <div class="prijs-boeken">
+                    <span class="title-block-alt">Totale prijs</span>
+                    <div class="totaal-prijs">€<?php echo $reis['Prijs'] ?> in total</div>
+                    <p>De prijs word berekend door de vlucht prijs + €15 administratie kosten</p>
+                </div>
+                <input class="action-button" type="submit" name="">
+            </div>
+        </form>
     </main>
 
 
---
+++ app/privacypolicy.php
@@ -0,0 +1,109 @@
+<?php 
+
+session_start();
+include_once 'includes/pdo.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Privacy policy</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+        <img src="img/logo-vaygo.png" alt="logo Vaygo">
+
+        <?php
+        include_once 'includes/header.php';
+        ?>
+
+    </header>
+
+    <main>
+        <div class="privacy-policy">
+            <span  class="title-block">Vaygo's policy</span>
+            <div class="last-update">
+                <span class="about-us-title">Laatste update aan onze policy</span>
+                <p>Vrijdag 12 juni 2026 om 12:28</p>
+            </div>
+            <div class="info-policy">
+                <span class="title-block-alt">Informatie die we verzamelen</span>
+                <p>Wanneer u onze website bezoekt, verzamelen wij:</p>
+                <ul>
+                    <li>Uw naam</li>
+                    <li>Uw email adres</li>
+                    <li>Uw IP adres</li>
+                    <li>Uw favoriete dinosaurus</li>
+                    <li>Uw grootste angsten</li>
+                    <li>Het laatste wat u om drie uur 's nachts heeft gegoogled.</li>
+                </ul>
+                <p>Als we het kunnen vinden, verzamelen we het.</p>
+                <span class="title-block-alt">Hoe gebruiken we uw gegevens</span>
+                <p>We gebruiken uw gegevens om:</p>
+                <ul>
+                    <li>Onze diensten te verbeteren</li>
+                    <li>Uw ervaring te personaliseren</li>
+                    <li>Uw muziek smaak te beoordelen</li>
+                    <li>Onze aandeelhouders tevreden te houden</li>
+                </ul>
+                <span class="title-block-alt">Delen van gegevens</span>
+                <p>Wij kunnen uw gegevens delen met:</p>
+                <ul>
+                    <li>Vertrouwde bedrijven </li>
+                    <li>Onbetrouwbare bedrijven</li>
+                    <li>Vreemdelingen in de bus</li>
+                    <li>Aliens die marktonderzoek uitvoeren</li>
+                    <li>De hoogste bieder</li>
+                </ul>
+                <span class="title-block-alt">Cookies</span>
+                <p>Onze website gebruikt cookies. Echte koekjes.</p>
+                <p>Elke keer dat u onze website bezoekt, eet een medewerker namens u een cookie. Dit helpt ons om de website optimaal te laten functioneren en houdt de sfeer op kantoor goed.</p>
+                <p>Alvast bedankt voor uw medewerking.</p>
+                <span class="title-block-alt">Beveiliging van gegevens</span>
+                <p>Wij beschermen uw gegevens met:</p>
+                <ul>
+                    <li>Wachtwoorden</li>
+                    <li>Firewalls</li>
+                    <li>Positief denken</li>
+                    <li>Een zeer agresieve gans die onze servers bewaakt</li>
+                </ul>
+                <p></p>
+                <span class="title-block-alt">Uw rechten</span>
+                <p>U heeft het recht om:</p>
+                <ul>
+                    <li>Uw gegevens op te vragen</li>
+                    <li>Uw gegevens te bewerken</li>
+                    <li>Uw gegevens te laten bewerken</li>
+                    <li>Doen alsof u nooit op deze website bent geweest</li>
+                </ul>
+                <p>Wij hebben het recht om u te nergeren</p>
+                <span class="title-block-alt">Diensten van derden</span>
+                <p>Wij maken mogelijk gebruik van diensten van derden om onze website te beheren.</p>
+                <p>Wij hebben hun privacybeleid waarschijnlijk ook niet gelezen.</p>
+                <span class="title-block-alt">Changes to our policy</span>
+                <p>Wij kunnen dit Privacybeleid op ieder moment aanpassen.</p>
+                <p>Bij grote wijzigingen zullen we waarschijnlijk vergeten erover te berichten, of gewoon hopen dat niemand het merkt.</p>
+            </div>
+            <img src="img/smiley.png" alt="smiley">
+        </div>
+    </main>
+
+    <footer>
+        
+    <?php
+    include_once 'includes/footer.php';
+    ?>
+    </footer>
+    
+</body>
+
+</html>
