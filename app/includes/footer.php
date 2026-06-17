3bf44c98518d231cc8720a6cb190cc8c56391757
EBakker08 <1218556@student.roc-nijmegen.nl>
2026-06-17 11:25:20 +0200

    Privacy policy button added to footer

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -3,7 +3,8 @@
         <a href="inlog.php">Login</a>
     </div>
     <div class="copyright">
-        <a href="privacypolicy.php"><p>Copyright &copy; 2026 Vaygo Travels. All rights reserved.</p></a>
+        <p>Copyright &copy; 2026 Vaygo Travels. All rights reserved.</p>
+        <div class="search-button"><a href="privacypolicy.php">Privacy policy</a></div>
     </div>
     <div class="button">
         <a href="contact.php">Contact</a>
