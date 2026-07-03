ec7bd5e0dd9545181938d2b5e5157f47df27abba
Harm-Stokman <harmstok2006@gmail.com>
2026-07-03 11:43:16 +0200

    aanpassing searchbar

--- app/reizen.php
+++ app/reizen.php
@@ -34,7 +34,7 @@ include_once 'includes/pdo.php';
                 <div>
                     <span class="title-block">Zoek jou vakantie</span>
                 <div class="find-vacations">
-                    <form class="searchbar" method="get" name="filterbar" action="reizen.php">
+                    <form class="form_searchbar" method="get" name="filterbar" action="reizen.php">
                         <select class="dropdown-selection" name="typevacation">
                             <option value="">Type vakantie</option>
                             <option value="Strand_en_zon">Strand en zon</option>
